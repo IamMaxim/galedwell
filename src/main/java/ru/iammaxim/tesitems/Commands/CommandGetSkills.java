99ca362b6ee9c7d225cf6faa48563da5c38527fa
IamMaxim <g.maxim.stepanoff@gmail.com>
2017-02-24 18:21:53 +0300

    Fixed getSkills command

--- src/main/java/ru/iammaxim/tesitems/Commands/CommandGetSkills.java
+++ src/main/java/ru/iammaxim/tesitems/Commands/CommandGetSkills.java
@@ -1,12 +1,12 @@
 package ru.iammaxim.tesitems.Commands;
 
-import com.mojang.realmsclient.gui.ChatFormatting;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import ru.iammaxim.tesitems.Player.IPlayerAttributesCapability;
 import ru.iammaxim.tesitems.TESItems;
 
@@ -30,7 +30,7 @@ public class CommandGetSkills extends CommandBase {
             IPlayerAttributesCapability cap = ((EntityPlayer) sender).getCapability(TESItems.attributesCapability, null);
             StringBuilder sb = new StringBuilder();
             for (String attr : TESItems.ATTRIBUTES) {
-                sb.append(ChatFormatting.BLUE).append(attr).append(": ").append(ChatFormatting.AQUA).append(cap.getAttribute(attr)).append("\n");
+                sb.append(TextFormatting.BLUE).append(attr).append(": ").append(TextFormatting.AQUA).append(cap.getAttribute(attr)).append("\n");
             }
             sender.addChatMessage(new TextComponentString(sb.toString()));
         }
